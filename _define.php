a81ac2d6eae409a9561d9efdf1db796c59f950c4
Johan Cwiklinski <johan@x-tnd.be>
2013-12-01 09:22:44 +0100

    Set 0.8 compatibility

--- _define.php
+++ _define.php
@@ -40,7 +40,7 @@ $this->register(
     'Full member card as PDF',  //Short description
     'Johan Cwiklinski',         //Author
     '1.4',                      //Version
-    '0.7.5',                    //Galette compatible version
+    '0.8',                      //Galette compatible version
     '2013-12-05',               //Release date
     null                        //Permissions needed - not yet implemented
 );
