17bd453b2cd22199065a27f5234b1c33326e20c1
Johan Cwiklinski <johan@x-tnd.be>
2015-04-26 06:47:31 +0200

    Translations

--- lang/lang_english.php
+++ lang/lang_english.php
@@ -1,108 +1,105 @@
 <?php
-// This file was automatically generated on Sun Feb 26 13:02:51 2012 (GMT)
+// This file was automatically generated on Sun Apr 26 04:47:02 2015 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../fullcard.php:169 
+// ../fullcard.php:164 
 $lang['City'] = 'City';
 
-// ../fullcard.php:207 
+// ../fullcard.php:202 
 $lang['At '] = 'At ';
 
 // ../templates/default/menu.tpl:4 
 $lang['Empty card'] = 'Empty card';
 
-// ../fullcard.php:181 
+// ../fullcard.php:176 
 $lang['Email adress'] = 'Email adress';
 
-// ../fullcard.php:70 
+// ../templates/default/public_menu.tpl:2 
+$lang['Printable subscription form in PDF'] = 'Printable subscription form in PDF';
+
+// ../fullcard.php:71 
 $lang['Adhesion form'] = 'Adhesion form';
 
 // ../templates/default/adh_actions.tpl:1 
 $lang['PDF member card'] = 'PDF member card';
 
-// ../fullcard.php:116 
+// ../fullcard.php:118 
 $lang['Politeness'] = 'Politeness';
 
-// ../fullcard.php:218 
+// ../fullcard.php:213 
 $lang['fullcard'] = 'fullcard';
 
-// ../fullcard.php:137 
+// ../fullcard.php:136 
 $lang['Company name'] = 'Company name';
 
-// ../fullcard.php:187 
+// ../fullcard.php:182 
 $lang['Username'] = 'Username';
 
-// ../fullcard.php:216 
+// ../fullcard.php:211 
 $lang['** Galette identifier, if applicable'] = '** Galette identifier, if applicable';
 
 // ../templates/default/menu.tpl:2 
 $lang['Full card'] = 'Full card';
 
-// ../fullcard.php:162 
+// ../fullcard.php:157 
 $lang['Zip Code'] = 'Zip Code';
 
-// ../fullcard.php:142 
+// ../fullcard.php:131 
 $lang['First name'] = 'First name';
 
-// ../fullcard.php:108 
+// ../fullcard.php:110 
 $lang['Donation'] = 'Donation';
 
-// ../fullcard.php:215 
+// ../fullcard.php:210 
 $lang['* Only for compagnies'] = '* Only for compagnies';
 
-// ../fullcard.php:148 
+// ../fullcard.php:143 
 $lang['Address'] = 'Address';
 
-// ../fullcard.php:132 
+// ../fullcard.php:126 
 $lang['Name'] = 'Name';
 
-// ../fullcard.php:175 
+// ../fullcard.php:170 
 $lang['Country'] = 'Country';
 
-// ../fullcard.php:104 
+// ../fullcard.php:106 
 $lang['Benefactor member'] = 'Benefactor member';
 
-// ../fullcard.php:93 
+// ../fullcard.php:95 
 $lang['Required membership:'] = 'Required membership:';
 
 // ../templates/default/adh_actions.tpl:1 
 $lang['%membername: PDF member card'] = '%membername: PDF member card';
 
-// ../fullcard.php:120 
-$lang['Mrs'] = 'Mrs';
-
-// ../fullcard.php:194 
+// ../fullcard.php:189 
 $lang['Amount'] = 'Amount';
 
 // ../templates/default/menu.tpl:4 
 $lang['Generates an empty member card you can print for events, for example'] = 'Generates an empty member card you can print for events, for example';
 
-// ../fullcard.php:99 
+// ../fullcard.php:101 
 $lang['Active member'] = 'Active member';
 
-// ../fullcard.php:210 
+// ../fullcard.php:205 
 $lang['Signature'] = 'Signature';
 
-// ../fullcard.php:203 
+// ../fullcard.php:198 
 $lang['Hereby, I agree to comply to %s association\'s statutes and its rules.'] = 'Hereby, I agree to comply to %s association\'s statutes and its rules.';
 
-// ../fullcard.php:125 
-$lang['Miss'] = 'Miss';
+// ../templates/default/public_menu.tpl:2 
+$lang['Subscribe PDF'] = 'Subscribe PDF';
 
-// ../fullcard.php:111 
+// ../fullcard.php:113 
 $lang['The minimum contribution for each type of membership are defined on the website of the association. The amount of donations are free to be decided by the generous donor.'] = 'The minimum contribution for each type of membership are defined on the website of the association. The amount of donations are free to be decided by the generous donor.';
 
 // ../templates/default/adh_fiche_action.tpl:2 
 $lang['Adherent file card'] = 'Adherent file card';
 
-// ../fullcard.php:80 
+// ../fullcard.php:81 
 $lang['Complete the following form and send it with your funds, in order to complete your subscription.'] = 'Complete the following form and send it with your funds, in order to complete your subscription.';
 
-// ../fullcard.php:130 
-$lang['Mister'] = 'Mister';
-
-// ../fullcard.php:208 
+// ../fullcard.php:203 
 $lang['On            /            /            '] = 'On            /            /            ';
 
 ?>
--- lang/lang_french.php
+++ lang/lang_french.php
@@ -1,108 +1,105 @@
 <?php
-// This file was automatically generated on Sun Feb 26 13:02:51 2012 (GMT)
+// This file was automatically generated on Sun Apr 26 04:47:02 2015 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../fullcard.php:169 
+// ../fullcard.php:164 
 $lang['City'] = 'Ville';
 
-// ../fullcard.php:207 
+// ../fullcard.php:202 
 $lang['At '] = 'Fait à ';
 
 // ../templates/default/menu.tpl:4 
 $lang['Empty card'] = 'Carte vide';
 
-// ../fullcard.php:181 
+// ../fullcard.php:176 
 $lang['Email adress'] = 'Courriel';
 
-// ../fullcard.php:70 
+// ../templates/default/public_menu.tpl:2 
+$lang['Printable subscription form in PDF'] = 'Formulaire de souscription imprimable au format PDF';
+
+// ../fullcard.php:71 
 $lang['Adhesion form'] = 'Formulaire d\'adhésion';
 
 // ../templates/default/adh_actions.tpl:1 
 $lang['PDF member card'] = 'Fiche adhérent PDF';
 
-// ../fullcard.php:116 
+// ../fullcard.php:118 
 $lang['Politeness'] = 'Civilité';
 
-// ../fullcard.php:218 
+// ../fullcard.php:213 
 $lang['fullcard'] = 'fiche_adherent';
 
-// ../fullcard.php:137 
+// ../fullcard.php:136 
 $lang['Company name'] = 'Raison sociale';
 
-// ../fullcard.php:187 
+// ../fullcard.php:182 
 $lang['Username'] = 'Identifiant';
 
-// ../fullcard.php:216 
+// ../fullcard.php:211 
 $lang['** Galette identifier, if applicable'] = '** Identifiant Galette, si applicable';
 
 // ../templates/default/menu.tpl:2 
 $lang['Full card'] = 'Fiche Adhérent';
 
-// ../fullcard.php:162 
+// ../fullcard.php:157 
 $lang['Zip Code'] = 'Code postal';
 
-// ../fullcard.php:142 
+// ../fullcard.php:131 
 $lang['First name'] = 'Prénom';
 
-// ../fullcard.php:108 
+// ../fullcard.php:110 
 $lang['Donation'] = 'Don';
 
-// ../fullcard.php:215 
+// ../fullcard.php:210 
 $lang['* Only for compagnies'] = '* Uniquement pour les personnes morales';
 
-// ../fullcard.php:148 
+// ../fullcard.php:143 
 $lang['Address'] = 'Adresse';
 
-// ../fullcard.php:132 
+// ../fullcard.php:126 
 $lang['Name'] = 'Nom';
 
-// ../fullcard.php:175 
+// ../fullcard.php:170 
 $lang['Country'] = 'Pays';
 
-// ../fullcard.php:104 
+// ../fullcard.php:106 
 $lang['Benefactor member'] = 'Membre bienfaiteur';
 
-// ../fullcard.php:93 
+// ../fullcard.php:95 
 $lang['Required membership:'] = 'Adhésion souhaitée :';
 
 // ../templates/default/adh_actions.tpl:1 
 $lang['%membername: PDF member card'] = '%membername: Fiche PDF';
 
-// ../fullcard.php:120 
-$lang['Mrs'] = 'Madame';
-
-// ../fullcard.php:194 
+// ../fullcard.php:189 
 $lang['Amount'] = 'Montant';
 
 // ../templates/default/menu.tpl:4 
 $lang['Generates an empty member card you can print for events, for example'] = 'Génère une fiche adhérent vierge que vous pourrez imprimer pour des évènements, par exemple';
 
-// ../fullcard.php:99 
+// ../fullcard.php:101 
 $lang['Active member'] = 'Membre actif';
 
-// ../fullcard.php:210 
+// ../fullcard.php:205 
 $lang['Signature'] = 'Signature';
 
-// ../fullcard.php:203 
+// ../fullcard.php:198 
 $lang['Hereby, I agree to comply to %s association\'s statutes and its rules.'] = 'Par la présente, je m\'engage à respecter les statuts ainsi que le règlement intérieur de l\'association %s.';
 
-// ../fullcard.php:125 
-$lang['Miss'] = 'Mademoiselle';
+// ../templates/default/public_menu.tpl:2 
+$lang['Subscribe PDF'] = 'Souscription PDF';
 
-// ../fullcard.php:111 
+// ../fullcard.php:113 
 $lang['The minimum contribution for each type of membership are defined on the website of the association. The amount of donations are free to be decided by the generous donor.'] = 'Les cotisations minimales pour chacun des types d\'adhésions sont fixées sur le site web de l\'association. Le montant des dons est quant à lui laissé à l\'entière appréciation des généreux donateurs.';
 
 // ../templates/default/adh_fiche_action.tpl:2 
 $lang['Adherent file card'] = 'Fiche adhérent';
 
-// ../fullcard.php:80 
+// ../fullcard.php:81 
 $lang['Complete the following form and send it with your funds, in order to complete your subscription.'] = 'Remplissez le formulaire suivant et joignez le lors de l\'envoi de votre règlement, pour compléter votre inscription.';
 
-// ../fullcard.php:130 
-$lang['Mister'] = 'Monsieur';
-
-// ../fullcard.php:208 
+// ../fullcard.php:203 
 $lang['On            /            /            '] = 'Le            /            /            ';
 
 ?>
