c79bdf17192aa268753a353ef494efa4302cea6b
Guillaume Rousse <guillomovitch@gmail.com>
2016-10-12 21:43:59 +0200

    override adhesion form URL on loading

---
+++ _preferences.php
@@ -0,0 +1,40 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Preferences override file for FullCard plugin
+ *
+ * PHP version 5
+ *
+ * Copyright © 2016 The Galette Team
+ *
+ * This file is part of Galette (http://galette.tuxfamily.org).
+ *
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Galette. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Plugins
+ * @package   GaletteFullcard
+ *
+ * @author    Johan Cwiklinski <johan@x-tnd.be>
+ * @copyright 2016 The Galette Team
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
+ * @version   SVN: $Id$
+ * @link      http://galette.tuxfamily.org
+ * @since     Available since 0.8.3 - 2016-10-15
+ */
+
+$_preferences = array(
+    'pref_adhesion_form_url' => 'plugins/'. $id .'/fullcard.php'
+);
--- templates/default/adh_fiche_action.tpl
+++
@@ -1,4 +0,0 @@
-      <li>
-        <a class="button" href="{$galette_base_path}{$fullcard_dir}fullcard.php?id_adh={$member->id}" id="btn_plugins_fullcard">{_T string="Adherent file card"}</a>
-      </li>
-
