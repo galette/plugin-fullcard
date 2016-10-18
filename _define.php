75f9848d23a100c726afd429093cc0359e4a87f9
Johan Cwiklinski <johan@x-tnd.be>
2016-10-18 21:42:48 +0200

    Missing route name

--- _define.php
+++ _define.php
@@ -40,6 +40,7 @@ $this->register(
     'Full member card as PDF',  //Short description
     'Johan Cwiklinski',         //Author
     '1.7dev',                   //Version
+    'fullcard',                 //routing name
     '0.9',                      //Galette compatible version
     '2016-09-18',               //Release date
     []                          //Permissions needed
