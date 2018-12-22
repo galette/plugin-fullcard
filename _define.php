eaab339a8c40e881d9965248c76f22e8de5c7dfc
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-12-22 20:53:35 +0100

    Bump version for 0.9.2 compat

--- _define.php
+++ _define.php
@@ -39,8 +39,8 @@ $this->register(
     'Galette Fullcard',         //Name
     'Full member card as PDF',  //Short description
     'Johan Cwiklinski',         //Author
-    '1.7.1',                    //Version
-    '0.9.1',                    //Galette compatible version
+    '1.7.2',                    //Version
+    '0.9.2',                    //Galette compatible version
     'fullcard',                 //routing name
     '2018-05-01',               //Release date
     []                          //Permissions needed
